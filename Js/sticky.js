7cf86caffd5db69cd30cc37b857e5fc5f1929f56
ryuji565656 <64273889+ryuji565656@users.noreply.github.com>
2020-09-17 00:04:51 +0900

    Update sticky.js

--- Js/sticky.js
+++ Js/sticky.js
@@ -1,17 +1,15 @@
 $(function(){
     $('.page-header').each(function () {
 
-        var $window = $(window), // ウィンドウを jQuery オブジェクト化
-            $header = $(this),   // ヘッダーを jQuery オブジェクト化
-            // ヘッダーのデフォルト位置を取得
+        var $window = $(window), // 
+            $header = $(this),
+            
             headerOffsetTop = $header.offset().top;
 
-        // ウィンドウのスクロールイベントを監視
-        // (ウィンドウがスクロールするごとに処理を実行する)
+
         $window.on('scroll', function () {
-            // ウィンドウのスクロール量をチェックし、
-            // ヘッダーのデフォルト位置を過ぎていればクラスを付与、
-            // そうでなければ削除
+            
+      
             if ($window.scrollTop() > headerOffsetTop) {
                 $header.addClass('sticky');
             } else {
@@ -19,8 +17,6 @@ $(function(){
             }
         });
 
-        // ウィンドウのスクロールイベントを発生させる
-        // (ヘッダーの初期位置を調整するため)
         $window.trigger('scroll');
 
     });
